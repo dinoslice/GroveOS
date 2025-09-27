81d4b4e3ac9887de5a1b25612d23de74818f76d8
MSKatKing <mskatking08@gmail.com>
2025-09-26 21:01:29 -0500

    Implement PageTable and add function stubs

--- modules/memory/src/page_table.rs
+++ modules/memory/src/page_table.rs
@@ -1,1 +1,22 @@
+use crate::{MemoryResult, VirtAddr};
+use crate::physical_memory::PhysAddr;
+
 pub struct PageTable(&'static mut [u64]);
+
+impl PageTable {
+    pub fn new() -> MemoryResult<Self> {
+        todo!()
+    }
+
+    pub fn install(&self) {
+        todo!()
+    }
+
+    pub fn map_page(&mut self, virt: VirtAddr, phys: PhysAddr) -> MemoryResult<()> {
+        todo!()
+    }
+
+    pub fn unmap_page(&mut self, virt: VirtAddr) -> MemoryResult<()> {
+        todo!()
+    }
+}
