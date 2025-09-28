0b45899a446c7a664e57a9dfd3c5cc6b5853fa60
MSKatKing <mskatking08@gmail.com>
2025-09-28 15:40:20 -0500

    Implement function to get virtual addresses of page tables using recursive page table mapping

--- modules/memory/src/page_table.rs
+++ modules/memory/src/page_table.rs
@@ -1,6 +1,13 @@
 use crate::{MemoryResult, VirtAddr};
 use crate::physical_memory::PhysAddr;
 
+const RECURSIVE_ENTRY: usize = 510;
+
+const PT_LEVEL_PML4: u8 = 3;
+const PT_LEVEL_PDPT: u8 = 2;
+const PT_LEVEL_PD: u8 = 1;
+const PT_LEVEL_PT: u8 = 0;
+
 pub struct PageTable(&'static mut [u64]);
 
 impl PageTable {
@@ -18,6 +25,26 @@ impl PageTable {
 
     fn get_table_or_create(&mut self, idx: usize) -> PageTable {
         todo!()
+    }
+
+    /// This function assumes that the requested page table is located in the overall page table structure
+    unsafe fn get_page_table(level: u8, indices: &[usize]) -> PageTable {
+        let mut addr = 0;
+        let mut shift: u8 = 39;
+
+        for _ in 0..level {
+            addr |= RECURSIVE_ENTRY << shift;
+            shift -= 9;
+        }
+
+        for index in indices {
+            addr |= index << shift;
+            shift -= 9;
+        }
+
+        unsafe {
+            Self(core::slice::from_raw_parts_mut(addr as *mut u64, 512))
+        }
     }
 
     pub fn map_page(&mut self, virt: VirtAddr, phys: PhysAddr) -> MemoryResult<()> {
