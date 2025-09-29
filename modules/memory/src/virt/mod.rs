4664a9b828325661cca54fc5ebf7ce6da03b15c3
MSKatKing <mskatking08@gmail.com>
2025-09-28 21:31:47 -0500

    Add new PageAllocator struct

--- modules/memory/src/virt/mod.rs
+++ modules/memory/src/virt/mod.rs
@@ -1,2 +1,13 @@
+use core::ptr::NonNull;
+use crate::page_table::PageTable;
+use crate::virt::node::Node;
+use crate::virt::node_allocator::NodeAllocator;
+
 mod node;
 mod node_allocator;
+
+pub struct PageAllocator {
+    node_allocator: &'static NodeAllocator,
+    root_node: NonNull<Node>,
+    pml4: PageTable,
+}
