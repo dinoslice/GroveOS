a3a46ce859e59301e749ff95042b32ef4f33ded6
MSKatKing <mskatking08@gmail.com>
2025-09-28 21:33:03 -0500

    Add function stubs for NodeAllocator

--- modules/memory/src/virt/node_allocator.rs
+++ modules/memory/src/virt/node_allocator.rs
@@ -1,4 +1,22 @@
+use core::ptr::NonNull;
+use crate::page_table::PageTable;
+use crate::virt::node::Node;
+
 pub struct NodeAllocator {
     bitmap: [u8; 1280],
     allocated_groups: u8,
+}
+
+impl NodeAllocator {
+    pub fn new(pml4: &mut PageTable) -> Self {
+        todo!()
+    }
+
+    pub fn allocate(&mut self) -> NonNull<Node> {
+        todo!()
+    }
+
+    pub fn deallocate(&mut self, node: NonNull<Node>) {
+        todo!()
+    }
 }
