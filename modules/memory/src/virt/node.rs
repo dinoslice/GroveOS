a14f12549f8af65883cb7610e8ec495f0f8bc0aa
MSKatKing <mskatking08@gmail.com>
2025-09-28 21:38:59 -0500

    Add helper functions to NodeStatus and PageRange

--- modules/memory/src/virt/node.rs
+++ modules/memory/src/virt/node.rs
@@ -18,9 +18,26 @@ pub struct PageRange {
     pub end_page: u32,
 }
 
+impl PageRange {
+    pub fn new(start_page: u32, end_page: u32) -> Self {
+        PageRange { start_page, end_page }
+    }
+
+    pub fn contains(&self, page: u32) -> bool {
+        page >= self.start_page && page < self.end_page
+    }
+}
+
 #[repr(u8)]
+#[derive(Copy, Clone, Eq, PartialEq)]
 pub enum NodeStatus {
     Free,
     Used,
     Reserved
+}
+
+impl NodeStatus {
+    pub fn can_use(&self) -> bool {
+        self == &NodeStatus::Free
+    }
 }
