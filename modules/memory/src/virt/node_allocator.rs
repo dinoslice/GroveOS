7359bc315a17789a974f03cfa10d0c892ce3727c
MSKatKing <mskatking08@gmail.com>
2025-10-01 20:34:18 -0500

    Restructure NodeAllocator and implement NodeAllocator::new

--- modules/memory/src/virt/node_allocator.rs
+++ modules/memory/src/virt/node_allocator.rs
@@ -1,15 +1,37 @@
 use core::ptr::NonNull;
 use crate::page_table::PageTable;
+use crate::physical_memory::PhysicalMemoryAllocator;
 use crate::virt::node::Node;
+use crate::{MemoryResult, VirtAddr};
 
 pub struct NodeAllocator {
-    bitmap: [u8; 128],
-    allocated_groups: u8,
+    bitmap: [u8; 320],
+    next_free: u64,
 }
 
 impl NodeAllocator {
-    pub fn new(pml4: &mut PageTable) -> Self {
-        todo!()
+    pub const VMEM_START: VirtAddr = 0xFFFF_FF00_0000_0000u64 as _;
+    pub const VMEM_SIZE: usize = 21;
+
+    pub fn new(pml4: &mut PageTable) -> MemoryResult<&'static mut Self> {
+        let allocator = PhysicalMemoryAllocator::get()?;
+
+        let node_allocator = allocator.allocate_page()?;
+        pml4.map_page(Self::VMEM_START, node_allocator)?;
+
+        for i in 1..=Self::VMEM_SIZE {
+            let page = PhysicalMemoryAllocator::get()?.allocate_page()?;
+            pml4.map_page(unsafe { Self::VMEM_START.add(i * 0x1000) }, page)?;
+        }
+
+        let node_allocator = unsafe {
+            &mut *Self::VMEM_START.cast::<Self>()
+        };
+
+        node_allocator.bitmap.fill(0);
+        node_allocator.next_free = 0;
+
+        Ok(node_allocator)
     }
 
     pub fn allocate(&mut self) -> NonNull<Node> {
